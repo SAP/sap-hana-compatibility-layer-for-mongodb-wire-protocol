4f0352aa859a0806c75b6b4580de599d6928a9d7
lucboj <lucasbojer@gmail.com>
2022-03-22 14:31:13 +0100

    the basics of delete works

--- internal/handlers/jsonb1/msg_delete.go
+++ internal/handlers/jsonb1/msg_delete.go
@@ -18,10 +18,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v4"
-
 	"github.com/FerretDB/FerretDB/internal/handlers/common"
-	"github.com/FerretDB/FerretDB/internal/pg"
 	"github.com/FerretDB/FerretDB/internal/types"
 	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/wire"
@@ -29,43 +26,55 @@ import (
 
 // MsgDelete deletes document.
 func (h *storage) MsgDelete(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
+	fmt.Println("msg")
+	fmt.Println(msg)
 	document, err := msg.Document()
 	if err != nil {
 		return nil, lazyerrors.Error(err)
 	}
-
+	fmt.Println("Document")
+	fmt.Println(document)
 	m := document.Map()
+	fmt.Println("m")
+	fmt.Println(m)
 	collection := m[document.Command()].(string)
-	db := m["$db"].(string)
+	//db := m["$db"].(string)
 	docs, _ := m["deletes"].(*types.Array)
-
+	fmt.Println("docs")
+	fmt.Println(docs)
 	var deleted int32
 	for i := 0; i < docs.Len(); i++ {
 		doc, err := docs.Get(i)
 		if err != nil {
 			return nil, lazyerrors.Error(err)
 		}
-
+		fmt.Println("doc")
+		fmt.Println(doc)
 		d := doc.(types.Document).Map()
+		fmt.Println("d")
+		fmt.Println(d)
+		sql := fmt.Sprintf(`DELETE FROM %s`, collection)
+		//var placeholder pg.Placeholder
 
-		sql := fmt.Sprintf(`delETE FROM %s`, pgx.Identifier{db, collection}.Sanitize())
-		var placeholder pg.Placeholder
-
-		elSQL, args, err := where(d["q"].(types.Document), &placeholder)
+		delSQL, args, err := whereHANA(d["q"].(types.Document))
 		if err != nil {
 			return nil, lazyerrors.Error(err)
 		}
-
-		limit, _ := d["limit"].(int32)
-		if limit != 0 {
-			sql += fmt.Sprintf(" WHERE _jsonb->'_id' IN (SELECT _jsonb->'_id' FROM %s", pgx.Identifier{db, collection}.Sanitize())
-			sql += elSQL
-			sql += " LIMIT 1)"
-		} else {
-			sql += elSQL
-		}
-
-		tag, err := h.hanaPool.ExecContext(ctx, sql, args...)
+		fmt.Println("delSQL")
+		fmt.Println(delSQL)
+		sql += delSQL
+		//limit, _ := d["limit"].(int32)
+		//if limit != 0 {
+		//	sql += fmt.Sprintf(" WHERE _jsonb->'_id' IN (SELECT _jsonb->'_id' FROM %s", pgx.Identifier{db, collection}.Sanitize())
+		//	sql += delSQL
+		//	sql += " LIMIT 1)"
+		//} else {
+		//	sql += delSQL
+		//}
+		sqlExec := fmt.Sprintf(sql, args...)
+		fmt.Println("sqlExec")
+		fmt.Println(sqlExec)
+		tag, err := h.hanaPool.ExecContext(ctx, sqlExec)
 		if err != nil {
 			// TODO check error code
 			return nil, common.NewErrorMessage(common.ErrNamespaceNotFound, "MsgDelete: ns not found: %w", err)
