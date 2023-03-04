65d3d4d74af87fa9764249f8e267265c6be7f1b5
lucboj <lucasbojer@gmail.com>
2023-03-04 12:43:49 +0100

    added sudo

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ hanaDriver/client/./hdbinst --batch --ignore=check_diskspace
 
 install_dir=$(dirname "${work_dir}")
 
-mv "${install_dir}"/sap/hdbclient/golang/src/SAP "${goroot}"/src/
+sudo mv "${install_dir}"/sap/hdbclient/golang/src/SAP "${goroot}"/src/
 
 cd "${install_dir}"/sap/hdbclient/golang/src
 
